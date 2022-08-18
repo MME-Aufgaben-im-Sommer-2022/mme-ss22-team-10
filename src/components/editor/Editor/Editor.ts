d9b4d8fb1c95e4c84b1add41d0a4b68f4c28a411
Alexander Weichart <>
2022-08-18 12:04:30 +0200

    updated: editor

--- src/components/editor/Editor/Editor.ts
+++ src/components/editor/Editor/Editor.ts
@@ -3,10 +3,13 @@ import html from "./Editor.html";
 import EditorBlock from "../EditorBlock/EditorBlock";
 import State from "../../../lib/state/State";
 import EditorModel from "../../../data/models/EditorModel";
+import EventBus from "../../../lib/events/EventBus";
+import { CLOSE_ALL_EDITOR_INPUTS_EVENT } from "../../../events/dataTypes/CloseAllEditorInputsEventData";
 
 export default class Editor extends WebComponent {
   editorModelState: State<EditorModel>;
 
+  $editor!: HTMLDivElement;
   $editorBlocksContainer!: HTMLDivElement;
 
   constructor(editorModelState: State<EditorModel>) {
@@ -19,6 +22,12 @@ export default class Editor extends WebComponent {
   }
 
   onCreate(): void {
+    this.$initHtml();
+    this.initListeners();
+  }
+
+  private $initHtml(): void {
+    this.$editor = this.select(".editor")!;
     this.$editorBlocksContainer = this.select(".editor-blocks-container")!;
     this.editorModelState.value.blockContents.forEach((_, index) => {
       const blockContentState = this.editorModelState.createSubState(
@@ -27,6 +36,12 @@ export default class Editor extends WebComponent {
       this.$editorBlocksContainer.appendChild(
         new EditorBlock(blockContentState)
       );
+    });
+  }
+
+  private initListeners(): void {
+    this.$editor.addEventListener("click", () => {
+      EventBus.notifyAll(CLOSE_ALL_EDITOR_INPUTS_EVENT, {});
     });
   }
 }
