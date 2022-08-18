a573be289295a34f0dd0c1f5d0fe7b235080ccc2
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-08-18 23:10:03 +0200

    adds env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_LOGGER_IS_ENABLED: string;
+  readonly VITE_API_ENDPOINT: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_DATABASE_ID: string;
+  readonly VITE_COLLECTION_NOTES: string;
+  readonly VITE_COLLECTION_TEMPLATES: string;
+  readonly VITE_TEST_USER_EMAIL: string;
+  readonly VITE_TEST_USER_PASSWORD: string;
   // more env variables...
 }
 
