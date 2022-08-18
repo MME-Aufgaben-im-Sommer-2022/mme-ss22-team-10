f939742e03444871ed95c908fdee841525b9c7ea
Alexander Weichart <>
2022-08-18 22:37:08 +0200

    alpha

--- src/components/Playground/Playground.ts
+++ src/components/Playground/Playground.ts
@@ -1,7 +1,12 @@
 import WebComponent from "../../lib/components/WebComponent";
 import html from "./Playground.html";
 import css from "./Playground.css";
-import Calendar from "../Calendar/Calendar/Calendar";
+import State from "../../lib/state/State";
+import Editor from "../Editor/Editor/Editor";
+import { info, log, trace } from "../../lib/utils/Logger";
+import { AppEvent } from "../../lib/events/AppEvent";
+import DataManager from "../../data/DataManager";
+import { BlockContentInputType } from "../../data/models/EditorModel";
 
 export default class Playground extends WebComponent {
   constructor() {
@@ -13,7 +18,65 @@ export default class Playground extends WebComponent {
   }
 
   onCreate(): void {
-    const calendar = new Calendar();
-    document.querySelector<HTMLDivElement>("#app")!.append(calendar);
+    // write your playground code here
+    // the dev playground won't be appended in production
+    const editorModel = DataManager.getEditorModel(new Date());
+
+    editorModel.then((editorModel) => {
+      const editorModelState = new State(editorModel),
+        blockContentsState = editorModelState.createSubState(
+          "value.blockContents"
+        ),
+        firstBlockInputState = new State("heyyy");
+
+      log(editorModelState, blockContentsState, firstBlockInputState);
+
+      editorModelState.addEventListener("change", (event: AppEvent) => {
+        log("editorModel changed", event.data);
+      });
+
+      blockContentsState.addEventListener("change", (event: AppEvent) => {
+        log("blockContentsState changed", event.data);
+      });
+
+      firstBlockInputState.addEventListener("change", (event: AppEvent) => {
+        log("inputState changed", event.data);
+      });
+
+      const button1 = this.select("#button-1"),
+        button2 = this.select("#button-2"),
+        button3 = this.select("#button-3");
+
+      button1?.addEventListener("click", () => {
+        editorModelState.value.blockContents[0].inputValue = "hello";
+      });
+
+      button2.addEventListener("click", () => {
+        blockContentsState.value[0].inputValue = "hello";
+
+        log("----- after events -----");
+        log(blockContentsState.value);
+        log(firstBlockInputState.value);
+      });
+
+      button3.addEventListener("click", () => {
+        editorModelState.value.blockContents[0].inputValue = "FROM DA TOP";
+      });
+
+      /*      const editor = new Editor(editorModelState);
+      this.select("div")!.appendChild(editor);
+      this.select("button")!.addEventListener("click", () => {
+        log("click");
+        editorModelState.value.blockContents[0] = {
+          inputType: BlockContentInputType.BulletPoint,
+          title: "Bullet Point",
+          inputValue: "",
+        };
+      });
+
+      editorModelState.addEventListener("change", (event: AppEvent) => {
+        info("editorModel changed", event.data);
+      });*/
+    });
   }
 }
--- src/lib/state/State.ts
+++ src/lib/state/State.ts
@@ -4,6 +4,7 @@ import {
   StateChangedEventData,
 } from "../../events/dataTypes/StateChangedEventData";
 import { log } from "../utils/Logger";
+import ObservableSlim from "observable-slim";
 
 // ====================================================== //
 // ====================== State ====================== //
@@ -15,143 +16,35 @@ import { log } from "../utils/Logger";
 // https://github.com/MME-Aufgaben-im-Sommer-2022/mme-ss22-team-10/blob/dev/docs/lib/State.md
 
 export default class State<T> extends Observable {
-  private val!: T;
-  private parentState: State<unknown> | null = null;
+  private val: T;
 
-  id = ""; // unique id that identifies any state
-  private static stateCounts: Map<string, number> = new Map<string, number>();
-
-  constructor(value: T) {
+  constructor(public val: T) {
     super();
-    this.generateId();
-    this.setValue(value);
-  }
-
-  private getTopState(): State<unknown> {
-    let topState = this.parentState;
-    while (topState && topState.parentState !== null) {
-      topState = topState.parentState;
-    }
-    return topState ? topState : this;
-  }
-
-  private setValue(_value: T): void {
-    const topState = this.getTopState(),
-      proxyMaker = topState ? topState.createProxy : this.createProxy;
+    log("State", "constructor", "val:", val);
     this.val =
-      typeof _value === "object"
-        ? proxyMaker(_value as Record<string, unknown>, "value")
-        : _value;
+      typeof val === "object"
+        ? ObservableSlim.create(val, false, this.onValueChange)
+        : val;
   }
-
-  private createProxy = (
-    value: Record<string, unknown>,
-    parentPropertyName: string
-  ): T => {
-    {
-      return new Proxy(value, this.proxyHandler(parentPropertyName)) as T;
-    }
-  };
-
-  private proxyHandler = (p: string) => {
-    return {
-      set: (object: any, key: string | symbol, value: any) => {
-        const wasTriggeredBySubState =
-            typeof key === "string" ? key.split("___").length > 1 : false,
-          triggerStateId = wasTriggeredBySubState
-            ? key.split("___")[0]
-            : this.id,
-          realKey = wasTriggeredBySubState
-            ? (key as string).split("___")[1]
-            : key;
-
-        const oldValue = object[realKey];
-        if (oldValue !== value) {
-          object[realKey] = value;
-
-          this.onChange({
-            oldPropertyValue: oldValue,
-            newPropertyValue: object[realKey],
-            propertyName: p + "." + realKey.toString(),
-            wasTriggeredBySubState: wasTriggeredBySubState,
-            triggerStateId: triggerStateId,
-            respondents: [triggerStateId],
-          });
-        }
-
-        return true;
-      },
-
-      // return new proxy for nested objects
-      // to avoid creating new proxies for already proxied objects, the following code was adopted from:
-      // https://stackoverflow.com/questions/41299642/how-to-use-javascript-proxy-for-nested-objects
-      get: (object: any, key: string | symbol) => {
-        if (key === "isProxy") {
-          return true;
-        }
-
-        if (key === "toJSON") {
-          return () => {
-            return object;
-          };
-        }
-
-        const prop = object[key];
-        if (typeof prop === "undefined") {
-          return undefined;
-        } else if (!prop["isProxy"]) {
-          if (typeof prop === "object") {
-            return this.createProxy(prop, p + "." + key.toString());
-          }
-        }
-
-        return prop;
-      },
-    };
-  };
 
   get value(): T {
     return this.val;
   }
 
-  set value(value: T) {
-    if (this.val !== value) {
-      const oldValue = this.val;
-      this.setValue(value);
-      this.onChange({
-        oldPropertyValue: oldValue,
-        newPropertyValue: this.val,
-        propertyName: "value",
-        wasTriggeredBySubState: false,
-        triggerStateId: this.id,
-        respondents: [this.id],
-      });
-      // this.parentState?.updateParentValue(
-      //   this.propNameInParentState!,
-      //   this.val
-      // );
+  set value(val: T) {
+    if (typeof val !== "object") {
+      this.val = val;
+    } else {
+      this.val = ObservableSlim.create(val, false, this.onValueChange);
     }
+    this.onValueChange({});
   }
 
-  private onChange = (data: StateChangedEventData) => {
-    this.notifyAll(STATE_CHANGE_EVENT, data);
+  onValueChange = (changes: any) => {
+    this.notifyAll("change", changes);
   };
 
-  private generateId = () => {
-    const name = this.constructor.name;
-    let count = State.stateCounts.get(name);
-    if (count === undefined) {
-      count = 0;
-    }
-    State.stateCounts.set(name, count + 1);
-    this.id = name + "-" + count;
-    Object.freeze(this.id);
-  };
-
-  // creates a new state from a property of this state.
-  // use this, if you want to create a new state from an existing state.
-  // e.g. exampleState.createSubState("value.someProperty") will create a new state from the value of the exampleState.value.someProperty property.
-  createSubState = (key: string): State<any> => {
+  createSubState(key: string): State<any> {
     const subStateKeys = key.split("."),
       subStateValue: any = subStateKeys.reduce((obj: any, key: string) => {
         const val = obj[key];
@@ -160,83 +53,8 @@ export default class State<T> extends Observable {
         }
         throw new InvalidStateKeyError(key, this);
       }, this),
-      subState = new State(subStateValue);
-
-    subState.setParentState(this, key);
-
-    const updateSubState = (event) => {
-        const data = event.data as StateChangedEventData,
-          isPropertyOfSubState = data.propertyName.startsWith(key),
-          propertyKeys = data.propertyName.split("."),
-          relativeKeys = [
-            propertyKeys[0],
-            ...propertyKeys.slice(subStateKeys.length, propertyKeys.length),
-          ].join("."),
-          subStateEventData = Object.assign({}, data, {
-            propertyName: relativeKeys,
-          });
-
-        if (
-          isPropertyOfSubState &&
-          (!data.respondents.includes(this.id) ||
-            data.triggerStateId === this.id)
-        ) {
-          if (data.wasTriggeredBySubState) {
-            if (data.triggerStateId !== this.id) {
-              // now we notify the substate
-              subStateEventData.respondents.push(this.id);
-              subState.notifyAll(STATE_CHANGE_EVENT, subStateEventData);
-            }
-          } else {
-            // now we notify the substate
-            subStateEventData.respondents.push(this.id);
-            subState.notifyAll(STATE_CHANGE_EVENT, subStateEventData);
-          }
-        }
-      },
-      onSubStateChanged = (event) => {
-        // the sub state changed, we need to do some updates
-        //log("sub state changed", event.data, this.id);
-        const data = event.data as StateChangedEventData;
-        //log("sub state changed", data, this.id);
-        if (
-          data.propertyName === "value" &&
-          !data.respondents.includes(this.id)
-        ) {
-          if (typeof data.oldPropertyValue !== "object") {
-            this.updateTopState(key, data);
-          } else {
-            //log("notifying all");
-            this.getTopState().notifyAll(STATE_CHANGE_EVENT, data);
-          }
-        }
-      };
-    this.addEventListener(STATE_CHANGE_EVENT, updateSubState);
-    subState.addEventListener(STATE_CHANGE_EVENT, onSubStateChanged);
-
+      subState = new State(subStateValue.__getTarget);
     return subState;
-  };
-
-  private updateTopState(propertyName: string, data: StateChangedEventData) {
-    let oldValue = undefined;
-    const props = propertyName.split(".");
-    props.shift();
-    try {
-      props.reduce((obj, prop, index) => {
-        if (index === props.length - 1) {
-          oldValue = obj[prop];
-          // set the value without triggering the
-          obj[data.triggerStateId + "___" + prop] = data.newPropertyValue;
-        }
-        return obj[prop];
-      }, this.val);
-    } catch (e) {
-      throw new ChangedParentStateError(propertyName, this, data);
-    }
-  }
-
-  private setParentState(state: State<unknown> | null, propName: string) {
-    this.parentState = state;
   }
 }
 
