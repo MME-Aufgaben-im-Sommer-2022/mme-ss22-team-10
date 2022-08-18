990322144159a79fc3b2892fd4f43e0358d99fad
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-08-18 23:10:28 +0200

    adds config for api

---
+++ src/data/api/config.ts
@@ -0,0 +1,9 @@
+export const Server = {
+  ENDPOINT: import.meta.env.VITE_API_ENDPOINT,
+  PROJECT_ID: import.meta.env.VITE_PROJECT_ID,
+  DATABASE_ID: import.meta.env.VITE_DATABASE_ID,
+  COLLECTION_NOTES: import.meta.env.VITE_COLLECTION_NOTES,
+  VITE_COLLECTION_TEMPLATES: import.meta.env.VITE_COLLECTION_TEMPLATES,
+  TEST_USER_EMAIL: import.meta.env.VITE_TEST_USER_EMAIL,
+  TEST_USER_PASSWORD: import.meta.env.VITE_TEST_USER_PASSWORD,
+};
