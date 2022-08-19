9e10c0b743efdf5ab3022d4e8ba97bb43435d751
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-08-19 05:00:47 +0200

    adds login functionalities

--- src/data/api/ApiClient.ts
+++ src/data/api/ApiClient.ts
@@ -16,4 +16,29 @@ export default class ApiClient {
     this.accountManager = new AccountManager(this.client);
     this.databaseManager = new DatabaseManager(this.client, Server.DATABASE_ID);
   }
+
+  static async logInUser() {
+    await this.accountManager
+      .createNewAccountSession(
+        Server.TEST_USER_EMAIL,
+        Server.TEST_USER_PASSWORD
+      )
+      .then((response) => {
+        // returns session Object : https://appwrite.io/docs/models/session
+        this.accountManager.sessionId = response.$id;
+        this.accountManager.userId = response.userId;
+      })
+      .then(async () => {
+        await this.accountManager.getAccountData().then(async (response) => {
+          this.accountManager.userName = response.name;
+        });
+      });
+  }
+
+  static async logOutUser(): Promise<any> {
+    return this.accountManager.deleteAccountSession();
+  }
+  static getUsername(): string {
+    return this.accountManager.userName;
+  }
 }
