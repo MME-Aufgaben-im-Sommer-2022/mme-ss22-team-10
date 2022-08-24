811fcee949fe8ec5831a78631f4711eb9e9ea959
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-08-24 03:02:59 +0200

    adds function to delete all notes from a user
    only for mock data atm

--- src/data/DataManager.ts
+++ src/data/DataManager.ts
@@ -111,6 +111,15 @@ export default class DataManager {
   // MOCK DATA
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
+  private static async deleteUserNotes() {
+    const userNotes: Array<any> = await ApiClient.getNoteDays();
+    userNotes.forEach(async (note) => {
+      await ApiClient.deleteEditorNotes(note.$id);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   private static async generateMockDatabaseData() {
     // eslint-disable-next-line no-magic-numbers
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].forEach((month) => {
