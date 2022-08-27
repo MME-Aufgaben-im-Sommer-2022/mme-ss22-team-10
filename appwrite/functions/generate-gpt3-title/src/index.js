08b22471071741f63c33c08c8f291133f1463a47
Alexander Weichart <>
2022-08-27 13:07:40 +0200

    added: appwrite test

--- appwrite/functions/generate-gpt3-title/src/index.js
+++ appwrite/functions/generate-gpt3-title/src/index.js
@@ -1,6 +1,13 @@
 const sdk = require("node-appwrite");
+const { Configuration, OpenAIApi } = require("openai");
+const { Query } = require("appwrite");
+// disable eslint
+/* eslint-disable */
+// disable jshint
+/* jshint ignore:start */
+
 // AFTER MAKING CHANGES TO THIS FILE:
-// run `appwrite deploy function` to deploy changes to Appwrite
+// run `appwrite deploy function` in the `appwrite/` directory to deploy changes to Appwrite
 // (make sure you installed appwrite via `npm install -g appwrite-cli`)
 
 /*
@@ -16,19 +23,12 @@ const sdk = require("node-appwrite");
   If an error is thrown, a response with code 500 will be returned.
 */
 
-module.exports = async function (req, res) {
+module.exports = async function(req, res) {
+  console.log("start executing function...");
   const client = new sdk.Client();
+  let generatedTitle = "";
+  console.log("created client");
 
-  // You can remove services you don't use
-  let account = new sdk.Account(client);
-  let avatars = new sdk.Avatars(client);
-  let database = new sdk.Databases(client, "YOUR_DATABASE_ID");
-  let functions = new sdk.Functions(client);
-  let health = new sdk.Health(client);
-  let locale = new sdk.Locale(client);
-  let storage = new sdk.Storage(client);
-  let teams = new sdk.Teams(client);
-  let users = new sdk.Users(client);
 
   if (
     !req.env["APPWRITE_FUNCTION_ENDPOINT"] ||
@@ -38,14 +38,95 @@ module.exports = async function (req, res) {
       "Environment variables are not set. Function cannot use Appwrite SDK."
     );
   } else {
+    const APPWRITE_FUNCTION_ENDPOINT = req.env["APPWRITE_FUNCTION_ENDPOINT"],
+      APPWRITE_FUNCTION_PROJECT_ID = req.env["APPWRITE_FUNCTION_PROJECT_ID"],
+      APPWRITE_FUNCTION_API_KEY = req.env["APPWRITE_FUNCTION_API_KEY"],
+      APPWRITE_FUNCTION_DATABASE_KEY = req.env["APPWRITE_FUNCTION_DATABASE_KEY"],
+      APPWRITE_FUNCTION_USER_ID = req.env["APPWRITE_FUNCTION_USER_ID"],
+      APPWRITE_FUNCTION_COLLECTION_NOTES = req.env["APPWRITE_FUNCTION_COLLECTION_NOTES"],
+      APPWRITE_FUNCTION_BLOCK_CONTENTS = req.env["APPWRITE_FUNCTION_BLOCK_CONTENTS"],
+      APPWRITE_FUNCTION_OPEN_AI_KEY = req.env["APPWRITE_FUNCTION_OPEN_AI_KEY"];
+
+    // log all
+    console.log(
+      "APPWRITE_FUNCTION_ENDPOINT: " + APPWRITE_FUNCTION_ENDPOINT,
+      "APPWRITE_FUNCTION_PROJECT_ID: " + APPWRITE_FUNCTION_PROJECT_ID,
+      "APPWRITE_FUNCTION_API_KEY: " + APPWRITE_FUNCTION_API_KEY,
+      "APPWRITE_FUNCTION_DATABASE_KEY: " + APPWRITE_FUNCTION_DATABASE_KEY,
+      "APPWRITE_FUNCTION_USER_ID: " + APPWRITE_FUNCTION_USER_ID,
+      "APPWRITE_FUNCTION_COLLECTION_NOTES: " + APPWRITE_FUNCTION_COLLECTION_NOTES,
+      "APPWRITE_FUNCTION_BLOCK_CONTENTS: " + APPWRITE_FUNCTION_BLOCK_CONTENTS,
+      "APPWRITE_FUNCTION_OPEN_AI_KEY: " + APPWRITE_FUNCTION_OPEN_AI_KEY
+    );
+
+    console.log("creating client...");
     client
       .setEndpoint(req.env["APPWRITE_FUNCTION_ENDPOINT"])
       .setProject(req.env["APPWRITE_FUNCTION_PROJECT_ID"])
       .setKey(req.env["APPWRITE_FUNCTION_API_KEY"])
       .setSelfSigned(true);
+    console.log("created client");
+    // OUR STUFF HERE
+    generatedTitle = await getTitleFromOpenAi(req, client);
+    console.log("generated title: " + generatedTitle);
   }
 
   res.json({
-    areDevelopersAwesome: true,
+    generatedTitle
   });
 };
+
+async function getBlockContents(req, client) {
+  let database = new sdk.Database(
+      client,
+      req.env["APPWRITE_FUNCTION_DATABASE_KEY"]
+    ),
+    userId = req.env["APPWRITE_FUNCTION_USER_ID"],
+    collectionNotes = req.env["APPWRITE_FUNCTION_COLLECTION_NOTES"],
+    collectionBlockContents = req.env["APPWRITE_FUNCTION_BLOCK_CONTENTS"];
+  blockContentArray = new Array();
+
+  const noteDocuments = await database.listDocuments(
+    collectionNotes,
+    [Query.equal("userID", userId)],
+    3
+  );
+  noteDocuments.documents.forEach((note) => {
+    blockContentDocuments = await database.listDocuments(
+      collectionBlockContents,
+      [Query.equal("noteID", note.$id)]
+    );
+    blockContentDocuments.forEach((blockContent) => {
+        blockContentArray.push(blockContent);
+      }
+    );
+  });
+  return blockContentArray;
+}
+
+async function getTitleFromOpenAi(req, client) {
+  const noteContents = "", blockContents = await getBlockContents(req, client),
+    OPENAI_API_KEY = req.env["APPWRITE_FUNCTION_OPEN_AI_KEY"];
+
+  blockContents.forEach((blockContent) => {
+    noteContents += blockContent.title + "\n";
+    noteContents += blockContent.inputValue + "\n\n";
+  });
+  const prompt = `These are the contents of your last 3 daily notes:\n"""\n${noteContents}"""\nComplete the following:\nRecently you were thinking about ..., what do you think about...?`;
+  const { Configuration, OpenAIApi } = require("openai");
+
+  const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY
+  });
+  const openai = new OpenAIApi(configuration);
+
+  return await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: prompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+}
