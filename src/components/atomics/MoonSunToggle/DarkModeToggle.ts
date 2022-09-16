ecc5cc0c790065c04c91509b4f12ba4c1a528d08
Alexander Weichart <>
2022-09-16 18:39:50 +0200

    implmented: dark mode

---
+++ src/components/atomics/MoonSunToggle/DarkModeToggle.ts
@@ -0,0 +1,80 @@
+/* eslint-disable no-magic-numbers */
+import WebComponent from "../../../lib/components/WebComponent";
+
+import css from "./DarkModeToggle.css";
+import lottie, { AnimationItem, AnimationSegment } from "lottie-web";
+import State from "../../../lib/state/State";
+import MoonSunToggleAnimationData from "./DarkModeToggleData.json";
+import { STATE_CHANGE_EVENT } from "../../../events/StateChanged";
+import DataManager from "../../../data/DataManager";
+
+const ToggleSegment = {
+  Light: [0, 50] as AnimationSegment,
+  Dark: [51, 96] as AnimationSegment,
+};
+
+// ADAPTED FROM: https://github.com/cawfree/react-dark-mode-toggle
+
+export default class DarkModeToggle extends WebComponent {
+  private readonly isCheckedState = new State(false);
+  private animationData!: AnimationItem;
+
+  constructor() {
+    super(undefined, css);
+  }
+
+  onCreate(): void | Promise<void> {
+    this.isCheckedState.value = DataManager.getDarkMode();
+    this.$initAnimation();
+    this.initListener();
+  }
+
+  get htmlTagName(): string {
+    return "moon-sun-toggle";
+  }
+
+  private $initAnimation(): void {
+    this.animationData = lottie.loadAnimation({
+      container: this,
+      animationData: MoonSunToggleAnimationData,
+      loop: false,
+      renderer: "svg",
+      autoplay: false,
+    });
+    this.animationData.setSpeed(1.4);
+
+    // go to dark mode if dark mode is enabled
+    if (this.isCheckedState.value) {
+      this.animationData.goToAndStop(51, true);
+    }
+
+    // update the display
+    this.updateDarkModeDisplay();
+  }
+
+  private initListener(): void {
+    this.select("svg > g")?.addEventListener("click", () => {
+      this.isCheckedState.value = !this.isCheckedState.value;
+    });
+
+    this.isCheckedState.addEventListener(STATE_CHANGE_EVENT, () => {
+      // after .5 seconds the animation will be done
+      setTimeout(() => this.updateDarkModeDisplay(), 300);
+      DataManager.setDarkMode(this.isCheckedState.value);
+      const segment = this.isCheckedState.value
+        ? ToggleSegment.Light
+        : ToggleSegment.Dark;
+      this.animationData.playSegments(segment, true);
+    });
+  }
+
+  private updateDarkModeDisplay(): void {
+    if (this.isCheckedState.value) {
+      document.body.classList.add("theme-dark");
+      document.body.classList.remove("theme-light");
+    } else {
+      document.body.classList.add("theme-light");
+      document.body.classList.remove("theme-dark");
+    }
+  }
+}
--- src/data/DataManager.ts
+++ src/data/DataManager.ts
@@ -239,6 +239,24 @@ export default class DataManager {
     return configModel;
   }
 
+  // Dark mode
+  static getDarkMode(): boolean {
+    const savedPreference = localStorage.getItem("darkMode");
+    if (savedPreference === null) {
+      // get from browser preference
+      const isDarkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      localStorage.setItem("darkMode", isDarkMode.toString());
+      return isDarkMode;
+    }
+    return savedPreference === "true";
+  }
+
+  static setDarkMode(darkMode: boolean): void {
+    localStorage.setItem("darkMode", darkMode.toString());
+  }
+
   // HELPER FUNCTIONS
   private static dayIsToday(day: Date) {
     const today = new Date();
