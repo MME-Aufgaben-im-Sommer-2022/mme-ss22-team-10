6775cac6f7e937d8848681ebc3069b168de43872
Alexander Weichart <>
2022-09-20 14:04:25 +0200

    added: comments to models

--- src/data/models/TemplateConfigurationModel.ts
+++ src/data/models/TemplateConfigurationModel.ts
@@ -1,11 +1,17 @@
 import Model from "../../lib/data/Model";
 import { TemplateItem } from "./UserSettingsModel";
 
+/**
+ * A single topic of the {@link TemplateConfigurationModel}
+ */
 export interface Topic {
   name: string;
   items: Array<TemplateItem>;
 }
 
+/**
+ * Model, which stores the available topics / items in the {@link TemplateConfigurator}
+ */
 export default class TemplateConfigurationModel extends Model {
   topics: Array<Topic>;
 
