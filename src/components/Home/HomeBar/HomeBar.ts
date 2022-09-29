27c64d5b5a30203b2ec929309855bdafc3ffd294
LMN <43175590+nekoumyu@users.noreply.github.com>
2022-09-29 12:19:15 +0200

    adds comments

--- src/components/Home/HomeBar/HomeBar.ts
+++ src/components/Home/HomeBar/HomeBar.ts
@@ -46,6 +46,10 @@ export default class HomeBar extends WebComponent {
     });
   }
 
+  /**
+   * set the {@link GlobalState} for the {@link UserSettingsModel user settings} if needed
+   * @private
+   */
   private async initData() {
     if (!GlobalState.hasState(GlobalStates.userSettingsModel)) {
       const userSettingsModel = await DataManager.getUserSettingsModel();
@@ -63,6 +67,9 @@ export default class HomeBar extends WebComponent {
     )!;
   }
 
+  /**
+   * initialize html elements
+   */
   $initHtml(): void {
     this.$greetText = this.select("#greet-text")!;
     this.$profileIcon = this.select("#profile-icon")!;
@@ -78,6 +85,9 @@ export default class HomeBar extends WebComponent {
       .build();
   }
 
+  /**
+   * initialize listeners for user related events
+   */
   initListener(): void {
     this.$manageAccOpt.addEventListener("click", this.onManageAccOptionClicked);
     this.$logoutOpt.addEventListener("click", this.$onLogoutOptionClicked);
@@ -89,6 +99,9 @@ export default class HomeBar extends WebComponent {
     );
   }
 
+  /**
+   * sign out the user and show a goodbye message
+   */
   private $onLogoutOptionClicked = async () => {
     await DataManager.signOut();
     new ToastFactory()
@@ -108,21 +121,33 @@ export default class HomeBar extends WebComponent {
     this.$setGreetText();
   };
 
+  /**
+   * set banner title
+   */
   $setGreetText(): void {
     this.$greetText.innerHTML = `🌱 Hello ${this.userSettingsModelState.value.username}!`;
   }
 
+  /**
+   * retrieve and set account information in the info box
+   */
   private $setAccInfo = async () => {
     const accountData = await DataManager.getAccountData();
     this.$email.innerHTML = accountData.email;
     this.$username.innerHTML = `🌱${accountData.name}`;
   };
 
+  /**
+   * toggle whether the account info box will be shown or hidden
+   */
   private $onProfileIconClicked = () => {
     this.$setAccInfo();
     this.$profileDropdown.classList.toggle("show");
   };
 
+  /**
+   * hide the account info box when clicked outside
+   */
   private $onFocusOut = () => {
     const target = event!.target;
     if (
